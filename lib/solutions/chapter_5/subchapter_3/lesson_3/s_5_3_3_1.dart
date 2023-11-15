1a54cd06470be3c1863a642f0c0cb6fbb99cee0e
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-15 16:40:50 +0100

    imgPath chnaged

--- lib/solutions/chapter_5/subchapter_3/lesson_3/s_5_3_3_1.dart
+++ lib/solutions/chapter_5/subchapter_3/lesson_3/s_5_3_3_1.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+
 class S5331 extends StatefulWidget {
   const S5331({Key? key}) : super(key: key);
   @override
   State<S5331> createState() => _S5331State();
 }
+
 class _S5331State extends State<S5331> {
   String imgPath = "";
   @override
@@ -21,7 +23,7 @@ class _S5331State extends State<S5331> {
                 final image =
                     await ImagePicker().pickImage(source: ImageSource.camera);
                 setState(() {
-                  imgPath = image?.path ?? "";
+                  imgPath = image!.path;
                 });
               },
               icon: const Icon(Icons.camera),
@@ -31,7 +33,7 @@ class _S5331State extends State<S5331> {
                 final image =
                     await ImagePicker().pickImage(source: ImageSource.gallery);
                 setState(() {
-                  imgPath = image?.path ?? "";
+                  imgPath = image!.path;
                 });
               },
               icon: const Icon(Icons.browse_gallery),
