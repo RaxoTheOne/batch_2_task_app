7c69f6d1e92c3ce437e1cf8ee9e53b3667cfb40e
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-15 12:55:05 +0100

    4.5.2.1. Aolution added

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
@@ -4,6 +4,8 @@ class S4521 extends StatelessWidget {
   const S4521({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Container(
+      child: Text('Hello World'),
+    );
   }
 }
