08e0a95f470d7d2239f73d57c28ca0f547544826
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-15 14:11:32 +0100

    Solution added 5.3.3.1.

--- lib/solutions/chapter_5/subchapter_3/lesson_3/s_5_3_3_1.dart
+++ lib/solutions/chapter_5/subchapter_3/lesson_3/s_5_3_3_1.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
-class S5331 extends StatelessWidget {
+class S5331 extends StatefulWidget {
   const S5331({Key? key}) : super(key: key);
   @override
+  State<S5331> createState() => _S5331State();
+}
+class _S5331State extends State<S5331> {
+  String imgPath = "";
+  @override
   Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      IconButton(
-          onPressed: () {
-          
-          },
-          icon: Icon(Icons.browse_gallery_sharp)),
-      ]
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            IconButton(
+              onPressed: () async {
+                final image =
+                    await ImagePicker().pickImage(source: ImageSource.camera);
+                setState(() {
+                  imgPath = image?.path ?? "";
+                });
+              },
+              icon: const Icon(Icons.camera),
+            ),
+            IconButton(
+              onPressed: () async {
+                final image =
+                    await ImagePicker().pickImage(source: ImageSource.gallery);
+                setState(() {
+                  imgPath = image?.path ?? "";
+                });
+              },
+              icon: const Icon(Icons.browse_gallery),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
