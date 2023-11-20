7fa7f15bdd668037ebfd93e84915c6b813c4e2f4
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-20 11:24:39 +0100

    fixed solution 5.1.3.1.

--- lib/solutions/chapter_5/subchapter_1/lesson_3/s_5_1_3_1.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_3/s_5_1_3_1.dart
@@ -5,31 +5,30 @@ class S5131 extends StatelessWidget {
   const S5131({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Google Fonts Demo'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'This is Roboto',
+          style: GoogleFonts.roboto(fontSize: 24),
+          selectionColor: Colors.black,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-            Text('This is Roboto',
-            style: GoogleFonts.roboto(fontSize: 24),
-            ),
-            Text('This is Pacifico',
-            style: GoogleFonts.pacifico(fontSize: 24),
-            ),
-            Text('This is Indie Flower',
-            style: GoogleFonts.indieFlower(fontSize: 24),
-            ),
-            Text('This is Lobster',
-            style: GoogleFonts.lobster(fontSize: 24),
-            ),
-            ],
-          )
+        Text(
+          'This is Pacifico',
+          style: GoogleFonts.pacifico(fontSize: 24),
+          selectionColor: Colors.black,
         ),
-      ),
+        Text(
+          'This is Indie Flower',
+          style: GoogleFonts.indieFlower(fontSize: 24),
+          selectionColor: Colors.black,
+        ),
+        Text(
+          'This is Lobster',
+          style: GoogleFonts.lobster(fontSize: 24),
+          selectionColor: Colors.black,
+        ),
+      ],
     );
   }
 }
