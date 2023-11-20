c421acfa57d44c6c2e448df73164648a0eb52019
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-20 12:41:02 +0100

    s_5.1.3.2. fixed key to super key

--- lib/solutions/chapter_5/subchapter_1/lesson_3/s_5_1_3_2.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_3/s_5_1_3_2.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
-
 class S5132 extends StatelessWidget {
   const S5132({super.key});
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: SizedBox(
-        width: 200,
-        height: 30,
-        child: Text(
-            'This is a very long text that should overflow the box and be clipped by the ClipRect widget.'),
-      ),
+      child: MyExampleBox(),
     );
+  }
+}
+class MyExampleBox extends StatefulWidget {
+  const MyExampleBox({super.key});
+  @override
+  State<MyExampleBox> createState() => _MyExampleBoxState();
+}
+class _MyExampleBoxState extends State<MyExampleBox> {
+    bool isExpanded = false;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+            width: 200,
+            height: 30,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isExpanded = !isExpanded;
+                });
+              },
+              child: Text(
+                "This is a very long text that should overflow the box and be clipped by the ClipRect widget.",
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+                overflow: isExpanded ? TextOverflow.visible : TextOverflow.ellipsis,
+              ),
+            ),
+          );
   }
 }
