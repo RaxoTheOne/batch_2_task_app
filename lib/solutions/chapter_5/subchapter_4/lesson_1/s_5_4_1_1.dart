e738ac49b67f6fd1b9c6849d7eb1e23490332870
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-21 11:26:55 +0100

    s_5.4.1.1. changed in good

--- lib/solutions/chapter_5/subchapter_4/lesson_1/s_5_4_1_1.dart
+++ lib/solutions/chapter_5/subchapter_4/lesson_1/s_5_4_1_1.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
 class S5411 extends StatelessWidget {
-  const S5411({super.key});
+  const S5411({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          try {
+            // Hier wird ein Laufzeitfehler erzeugt
+            throw Exception('[meinException-Name]');
+          } catch (e) {
+            // Hier wird der Fehler in einer SnackBar ausgegeben
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Fehler aufgetreten: $e'),
+              ),
+            );
+          }
+        },
+        child: Text('Klicke mich'),
+      ),
+    );
   }
 }
