89431ba85f2785342cf35b86c55b17944ba133fe
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-21 11:03:43 +0100

    make new

--- lib/solutions/chapter_5/subchapter_4/lesson_1/s_5_4_1_1.dart
+++ lib/solutions/chapter_5/subchapter_4/lesson_1/s_5_4_1_1.dart
@@ -4,50 +4,6 @@ class S5411 extends StatelessWidget {
   const S5411({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter-Code'),
-      ),
-      body: Center(
-        child: MyButton(),
-      ),
-    );
-  }
-}
-
-class MyButton extends StatefulWidget {
-  @override
-  _MyButtonState createState() => _MyButtonState();
-}
-
-class _MyButtonState extends State<MyButton> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  void _showSnackBar(String message) {
-    final snackBar = SnackBar(content: Text(message));
-    // Verwende die neue Methode, um eine SnackBar anzuzeigen
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-  }
-
-  void _handleButtonPress() {
-    try {
-      // Erzeuge einen Laufzeitfehler, indem du eine Null-Variable verwendest
-      var x = null;
-      var y = x + 1;
-    } catch (e) {
-      // Fange den Fehler ab und gib ihn in einer SnackBar aus
-      _showSnackBar(e.toString());
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      body: ElevatedButton(
-        child: Text('Klicke mich'),
-        onPressed: _handleButtonPress,
-      ),
-    );
+    throw UnimplementedError();
   }
 }
