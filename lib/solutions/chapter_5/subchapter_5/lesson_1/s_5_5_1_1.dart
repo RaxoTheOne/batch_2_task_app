e2f61a97ea60fc8f0e281af7a573089feb468425
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-27 11:09:07 +0100

    s_5.5.1.1. fertig gemacht taskList s_5.5.1.1. solutions added

--- lib/solutions/chapter_5/subchapter_5/lesson_1/s_5_5_1_1.dart
+++ lib/solutions/chapter_5/subchapter_5/lesson_1/s_5_5_1_1.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
 
-class S5511 extends StatelessWidget {
-  const S5511({super.key});
+class S5511 extends StatefulWidget {
+  const S5511({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError();
+  _S5511State createState() => _S5511State();
+}
+
+class _S5511State extends State<S5511> {
+  int counter1 = 0;
+  int counter2 = 0;
+
+  Future<void> warteKurz() async {
+    await Future.delayed(const Duration(milliseconds: 500));
   }
 
-  // ignoriere für den Moment den Inhalt dieser Methode
-  // nutze "await warteKurz();" in deinem Code, um einen Moment zu warten
-  Future<void> warteKurz() {
-    return Future.delayed(const Duration(milliseconds: 500));
+  Future<void> asyncMethode1() async {
+    await warteKurz();
+    print('Methode 1: $counter1');
+    setState(() {
+      counter1++;
+    });
+  }
+
+  Future<void> asyncMethode2() async {
+    await warteKurz();
+    print('Methode 2: $counter2');
+    setState(() {
+      counter2++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              await asyncMethode1();
+              await asyncMethode2();
+              print('Fertig');
+            },
+            child: Text('Click me'),
+          ),
+          SizedBox(height: 20),
+          Text('Counter 1: $counter1'),
+          Text('Counter 2: $counter2'),
+        ],
+      ),
+    );
   }
 }
