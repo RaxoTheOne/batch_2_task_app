a41ae0f93624ef8faafd290bd8ad90f5f2f1a667
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-28 18:51:59 +0100

    s_4.5.3.3. added taskList updated

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
@@ -4,6 +4,16 @@ class S4533 extends StatelessWidget {
   const S4533({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Container(
+      width: 150,
+      height: 150,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('App'),
+          Text('Akademie'),
+        ],
+      ),
+    );
   }
 }
