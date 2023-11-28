95bac08703bfbc4c07846d1e2f7027a26e365d09
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-28 09:50:39 +0100

    s_5.5.2.4. changed better then before

--- lib/solutions/chapter_5/subchapter_5/lesson_2/s_5_5_2_4.dart
+++ lib/solutions/chapter_5/subchapter_5/lesson_2/s_5_5_2_4.dart
@@ -9,6 +9,9 @@ class S5524 extends StatefulWidget {
 }
 
 class _S5524State extends State<S5524> {
+  // Erstellen Sie eine Variable, um die Future zu speichern
+  Future<String>? diceFuture;
+
   Future<String> _rollDice() async {
     // Simuliere das Würfeln mit einer Verzögerung von 2 Sekunden
     await Future.delayed(Duration(seconds: 2));
@@ -31,20 +34,26 @@ class _S5524State extends State<S5524> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              // Erstellen Sie eine neue Future, wenn Sie die Schaltfläche drücken
+              diceFuture = _rollDice();
+              // Aktualisieren Sie den Zustand des Widgets, um die FutureBuilder neu zu erstellen
+              setState(() {});
+            },
             child: Text('Würfeln'),
           ),
           SizedBox(height: 16),
           FutureBuilder(
-            future: _rollDice(),
-            builder: (context, snapshot) {
+            // Verwenden Sie die Future-Variable, die Sie erstellt haben
+            future: diceFuture,
+            builder: (context, AsyncSnapshot<String> snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return Text('Würfel wird geworfen...');
               } else if (snapshot.hasError) {
                 return Text('Fehler: ${snapshot.error}');
               } else {
                 return Text(
-                  snapshot.data as String,
+                  snapshot.data!,
                   style: TextStyle(fontSize: 18),
                 );
               }
